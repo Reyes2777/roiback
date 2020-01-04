d266ac605423a1f3cc54fdb197df71ad6892193c
Jonathan Reyes <jonathan.reyes2777@gmail.com>
2020-01-04 09:55:34 -0500

    add form for filters, add model reservation, modify fields for room, and hotel

--- booking/models.py
+++ booking/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from .utils import ROOM_TYPES, LOCATION_CHOICES, CAPACITY_ROOMS
 
+from accounts.models import CustomUser
+
 
 class Hotel(models.Model):
     name = models.CharField(max_length=50)
@@ -11,6 +13,7 @@ class Hotel(models.Model):
     unlikes = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
     updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return '{} '.format(self.name)
@@ -24,5 +27,16 @@ class Room(models.Model):
     hotel = models.ForeignKey(Hotel, default=1, verbose_name='Hotel', on_delete=models.SET_DEFAULT)
     number_room = models.CharField(max_length=4)
     description = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)
     capacity = models.CharField(max_length=2, choices=CAPACITY_ROOMS)
     room_type = models.CharField(max_length=30, choices=ROOM_TYPES)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')
+
+
+class Reservation(models.Model):
+    date_of_enter = models.DateField(verbose_name='Fecha de Entrada')
+    date_of_exit = models.DateField(verbose_name='Fecha de Salida')
+    reserved_room = models.ForeignKey(Room, default=1, verbose_name='Room', on_delete=models.SET_DEFAULT)
+    user = models.ForeignKey(CustomUser, default=1, verbose_name='User', on_delete=models.SET_DEFAULT)
