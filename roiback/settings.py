ced64c06f7a27cfccf1c27d9e1bc1c4ab2f9f313
Jonathan Reyes <jonathan.reyes2777@gmail.com>
2020-01-06 18:04:01 -0500

    fix debug

--- roiback/settings.py
+++ roiback/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a248s2ei2&kt@)nouzfcq(j6-k&*3p1+!me_28bk=a22g6a42*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
